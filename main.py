dfa0f89667c3d01c77cc6be9d960829ccaaf2843
Hanna Mägi <hanna.magi.001@kgk.ee>
2022-03-31 10:41:31 +0300

    Küsime kauba kogust [Delivers #181737430]

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello World")
 print("Kui sooritate ostu, mis on rohkem või võrdne 5000$, saate lisa soodustust!!")
 kauba_valik = input("Sisestage kaup, mida soovite osta: ")
+kauba_kogus = int(input("Sisestage kauba kogus: "))
