4febb8b7b870db9a6f3ad4c9395795a4b5b4e72b
Hanna Mägi <hanna.magi.001@kgk.ee>
2022-03-31 10:46:58 +0300

    Lisame osariikide käibemaksud [Delivers #181737609]

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ print("Kui sooritate ostu, mis on rohkem või võrdne 5000$, saate lisa soodustu
 kauba_valik = input("Sisestage kaup, mida soovite osta: ")
 kauba_kogus = int(input("Sisestage kauba kogus: "))
 asukoht = input("Sisestage State(ut, nv, tx, al, ca): ").upper()
+
+if asukoht == "UT":
+    asukoht = 0.0685
+elif asukoht == "NV":
+    asukoht = 0.08
+elif asukoht == "TX":
+    asukoht = 0.0625
+elif asukoht == "AL":
+    asukoht = 0.04
+elif asukoht == "CA":
+    asukoht = 0.0825
